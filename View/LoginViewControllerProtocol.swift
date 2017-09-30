2eef6825044e4d7e45520bae4b2de84d5d35eda3
Jakob Grumsen <jakob@grumsenit.dk>
2017-09-30 12:33:59 +0200

    Add Router to LoginViewControllerProtocol

--- View/LoginViewControllerProtocol.swift
+++ View/LoginViewControllerProtocol.swift
@@ -3,4 +3,6 @@ import Foundation
 protocol LoginViewControllerProtocol: class {
     var eventHandler: LoginEventHandlerProtocol { get }
     var viewModel: LoginViewModel? { get set }
+    var router: (NSObjectProtocol & LoginRouterProtocol & LoginDataPassing)?
+
 }
