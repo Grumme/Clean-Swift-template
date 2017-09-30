1df0bd023dfdfe3a56e8d5b01da2bceb3d758141
Jakob Grumsen <jakob@grumsenit.dk>
2017-09-30 12:34:25 +0200

    Fix

--- View/LoginViewControllerProtocol.swift
+++ View/LoginViewControllerProtocol.swift
@@ -3,6 +3,6 @@ import Foundation
 protocol LoginViewControllerProtocol: class {
     var eventHandler: LoginEventHandlerProtocol { get }
     var viewModel: LoginViewModel? { get set }
-    var router: (NSObjectProtocol & LoginRouterProtocol & LoginDataPassing)?
+    var router: (NSObjectProtocol & LoginRouterProtocol & LoginDataPassing)? { get set }
 
 }
