1345acfba7c1797c778e7d44a76c8e61d2a33388
schemabuoi <charyszczakg@gmail.com>
2021-07-01 23:13:10 +0200

    Update Component Template & Add test version of CollectionViewCell Template

--- Login/Presenter/LoginEventHandlerProtocol.swift
+++ Login/Presenter/LoginEventHandlerProtocol.swift
@@ -7,6 +7,9 @@ import Foundation
 import UIKit
 
 protocol LoginEventHandlerProtocol {
+    func didLoad()
     func willAppear()
+    func traitCollectionDidChange()
+
     func prepare(for segue: UIStoryboardSegue)
 }
